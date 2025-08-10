8b5ae81396f59d4a1a742819d2974d524d5f3190
bsv15 <bomminenisrinivasulu02@gmail.com>
2025-08-11 00:11:06 +0530

    Uploaded on 2025-08-10 18:41:04.252Z

---
+++ Submissions/abc/c/basics_1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    // Write your code here
+int age = 30;
+float h = 1.75;
+printf("My age is %d years and my height is %.2f meters.",age,h);    
+    return 0;
+}
