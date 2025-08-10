67d10222278f624dea159484ce5e0efb3acacbee
bsv15 <bomminenisrinivasulu02@gmail.com>
2025-08-11 00:05:44 +0530

    Uploaded on 2025-08-10 18:35:42.285Z

--- Submissions/abc/c/basics_0.c
+++ Submissions/abc/c/basics_0.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    // Write your code here this is 2nd 
+    // Write your code here this is 23nd 
     printf("Welcome to C Programming!");   
     return 0;
 }
