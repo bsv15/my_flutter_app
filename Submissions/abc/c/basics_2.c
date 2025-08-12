f44299174446ff4c95dac76a6b51d2455ce1752d
bsv15 <bomminenisrinivasulu02@gmail.com>
2025-08-12 18:11:40 +0530

    Uploaded on 2025-08-12 12:41:37.169Z

---
+++ Submissions/abc/c/basics_2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2;
+    scanf("%d%d", &num1, &num2);
+    // Write your code here
+printf("Sum: %d\n",num1+num2);
+printf("Difference: %d\n",num1-num2);
+printf("Product: %d\n",num1*num2);
+    return 0;
+}
